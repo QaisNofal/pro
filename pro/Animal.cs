5c9a7d5d623c62e90ad9edc69bf1e9fe5c896a36
Qais <qusaiar44@gmail.com>
2024-01-23 14:34:30 +0300

    Initial commit

---
+++ pro/Animal.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace pro
+{
+    public abstract class  Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public abstract void MakeVoice();
+       
+
+    }
+
+    public class Dog : Animal
+    {
+     
+        public Dog(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+
+       
+        public override void MakeVoice()
+        {
+            Console.WriteLine($"{this.GetType().Name} says hohoho. Age: {Age}, Name: {Name}");
+        }
+    }
+
+    public class Cat : Animal
+    {
+      
+        public Cat(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+
+        
+        public override void MakeVoice()
+        {
+            Console.WriteLine($"{this.GetType().Name} says meeaw. Age: {Age}, Name: {Name}");
+        }
+    }
+    }
---
+++ pro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace pro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog("Buddy", 5);
+            Cat myCat = new Cat("Whiskers", 3);
+
+           
+            myDog.MakeVoice();
+            myCat.MakeVoice();
+        }
+    }
+}
